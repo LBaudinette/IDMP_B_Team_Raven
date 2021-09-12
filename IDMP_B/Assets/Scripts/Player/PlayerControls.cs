bd0edd9e5ea91a7888854b52ee243f71bca07c95
Louis Baudinette (s3668025) <s3668025@student.rmit.edu.au>
2021-09-12 18:42:11 +1000

    some code + placeholder code for player movement

--- IDMP_B/Assets/Scripts/Player/PlayerControls.cs
+++ IDMP_B/Assets/Scripts/Player/PlayerControls.cs
@@ -4,19 +4,21 @@ using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
 {
-    int xPos;
-    int zPos;
-    private GridXZ<GridObject> grid;
+    Vector3 pos;
+    public GridXZ<GridObject> grid;
     // Start is called before the first frame update
     void Start()
     {
-        
+        pos.x = 0;
+        pos.y = 0;
+        pos.z = 0;
+        UpdateGridPos();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CheckMousePos();
     }
 
     void CheckMousePos()
@@ -27,12 +29,22 @@ public class PlayerControls : MonoBehaviour
         // if player inputs left click to move
         if (Input.GetMouseButtonDown(0))
         {
+            Vector3 gridPos = grid.GetXZCell(mousePos);
+            GridObject node = grid.GetGridObject(mousePos);
             //if grid node at mouse position is adjacent to player's current position
-            if (true)
+            if (Mathf.Abs(gridPos.x - pos.x) == 1 && Mathf.Abs(gridPos.z - pos.z) == 1)
             {
                 //update player's position to adjacent grid node
-                
+                pos.x = gridPos.x;
+                pos.z = gridPos.z;
+                UpdateGridPos();
+
             }
         }
+    }
+
+    void UpdateGridPos()
+    {
+        // update gameobject transform to (x * cellSize + cellSize / 2, y, z * cellSize + cellSize / 2)
     }
 }
